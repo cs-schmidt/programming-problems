b93ab04955afae5c779076aa41681e896cdb35b0
cs-schmidt <ebs.euler@gmail.com>
2023-04-22 17:31:49 -0600

    saving problem progress

--- leetcode/daily-challenges/2023-04-19_daily-662.ts
+++ leetcode/daily-challenges/2023-04-19_daily-662.ts
@@ -3,131 +3,165 @@
 
 /**
  * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
+class TreeNode {
+     val: number
+     left: TreeNode | null
+     right: TreeNode | null
+     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+     }
+ }
  */
 
-class OrderedTriple {
-	constructor(x: any, y: any, z: any) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
+/** A class for storing tree nodes and their associated level in a btree. */
+class TreeNodeData {
+	node: TreeNode | null;
+	level: number;
+	excludedNodes: number;
+	constructor(node: TreeNode | null, level: number, excludedNodes: number) {
+		this.node = node;
+		this.level = level;
+		this.excludedNodes = excludedNodes;
 	}
-	public x: any = null;
-	public y: any = null;
-	public z: any = null;
-}
-
-/** The maximum width at a level if the btree were complete.  */
-function maxWidthOfLevel(level: number): number {
-	if (!Number.isInteger(level) || level < 1)
-		throw RangeError(
-			`Variable \`level\` must be a positive integer, got ${level} instead.`
-		);
-	return Math.pow(2, level - 1);
 }
 
 function widthOfBinaryTree(root: TreeNode | null): number {
-	let level: number = 1; // deepest level reached thus far
-	let lNode: TreeNode | null = root.left;
-	let rNode: TreeNode | null = root.right;
-	let lExcludedNodes: number = 0;
-	let rExcludedNodes: number = 0;
-	const lParents: OrderedTriple[] = [new OrderedTriple(root, level, 0)];
-	const rParents: OrderedTriple[] = [new OrderedTriple(root, level, 0)];
-	let maxWidth: number = 1; // greatest width thus seen down to the deepest level reached so far
+	let level: number = 1; // the current level in the btree we're assessing.
+	let lNode: TreeNode | null = root; // the left-most end-node at the given level.
+	let rNode: TreeNode | null = root; // the right-most end-node at the given level.
+	let lExcludedNodes: number = 0; // the number of nodes to the left of the left-most node at the current level.
+	let rExcludedNodes: number = 0; // the number of nodes to the right of the right-most node at the current level.
+	let lParents: TreeNodeData[] = []; // a stack of ancestor nodes to `lNode` with untraversed children.
+	let rParents: TreeNodeData[] = []; // a stack of ancestor nodes to `rNode` with untraversed children.
+	let mWidth: number = 1; // tracks the largest width encountered as analysis continue.
 
-	// When the root has no children.
-	if (!root.left && !root.right) return maxWidth;
-
-	// Start by finding the level with it's end-nodes maximally spread apart, and
-	// get the max width for that level.
+	// We initialize `lNode` and `rNode` to the deepest level with a width equal
+	// to what it would be in a complete btree. And, add any nodes we find with
+	// untraversed children down to that level to the stacks `lParents` and
+	// `rParents`.
 	while (lNode?.left && rNode?.right) {
-		level += 1;
-		if (lNode.right) lParents.push(new OrderedTriple(lNode, level, 0));
-		if (rNode.left) rParents.push(new OrderedTriple(rNode, level, 0));
+		if (hasBothChildren(lNode))
+			lParents.push(new TreeNodeData(lNode, level, 0));
+		if (hasBothChildren(rNode))
+			rParents.push(new TreeNodeData(rNode, level, 0));
 		lNode = lNode.left;
 		rNode = rNode.right;
+		level += 1;
 	}
+	mWidth = maxWidthOfLevel(level, lExcludedNodes, rExcludedNodes);
+	console.log(mWidth);
+	console.log(lExcludedNodes + ' ' + rExcludedNodes);
 
-	maxWidth = Math.max(
-		maxWidth,
-		maxWidthOfLevel((level += 1)) - (lExcludedNodes + rExcludedNodes)
-	);
-	let lNodeLevel: number = lParents[lParents.length - 1][1];
-	let rNodeLevel: number = rParents[rParents.length - 1][1];
-
-	// While `lNode` and `rNode` don't share their most recent ancestor with more
-	// than one child, find the max width of `level += 1`:
-
-	while (!(lNode === rNode && !lNode.left && !lNode.right)) {
-		// Find the left-most end-node at `level += 1` and calculate the number of
-		// end-nodes that would have been to the left of this node (on its level);
-		// `lExcludedNodes`.
-		while (lNodeLevel < level + 1) {
-			if (lNode.left && lNode.right) {
-				// Current node has two children.
-				lParents.push(new OrderedTriple(lNode, level, lExcludedNodes));
+	// Now either `lNode` doesn't have a left child or `rNode` doesn't have a
+	// right child. With `lNode` and `rNode` initialized, we advance `lNode` to
+	// the next left-most node in `level += 1` and `rNode` to the next right-most
+	// node in `level += 1`. Then, we calculate the width at `level += 1`. The
+	// procession of this cycle stops when...
+	let lNodeLevel: number;
+	let rNodeLevel: number;
+	while (lNode !== rNode || lNode.left || lNode.right) {
+		// Move `lNode` to the next left-most node in `level += 1`;
+		// *************************************************************************
+		lNodeLevel = level;
+		while (lNode != rNode && lNodeLevel < level + 1) {
+			// When `lNode` has both children.
+			if (hasBothChildren(lNode)) {
+				lParents.push(new TreeNodeData(lNode, lNodeLevel, lExcludedNodes));
 				lNode = lNode.left;
+				lNodeLevel += 1;
 				lExcludedNodes *= 2;
-			} else if (lNode.left) {
-				// Current node has only left child.
+			}
+			// When `lNode` has only the left child.
+			else if (lNode.left) {
 				lNode = lNode.left;
+				lNodeLevel += 1;
 				lExcludedNodes *= 2;
-			} else if (lNode.right) {
-				// Current node has only right child.
+			}
+			// When `lNode` has only the right child.
+			else if (lNode.right) {
 				lNode = lNode.right;
+				lNodeLevel += 1;
 				lExcludedNodes = 2 * lExcludedNodes + 1;
-			} else {
-				// Backtrack to last ancestor node with children untraveresed and begin
-				// left-biased traversal to find the next left-most node of `level += 1`.
-				lNodeLevel = lParents[lParents.length - 1].y;
-				lNode = lParents.pop().x.right;
-				lExcludedNodes = 1;
+			}
+			// When `lNode` has no children.
+			else {
+				// Set `lNode` to the last ancestor node with an untraversed child and
+				// `lNodeLevel` to the level of that node.
+				lNodeLevel = lParents.at(-1).level;
+				lExcludedNodes = lParents.at(-1).excludedNodes;
+				lNode = lParents.pop().node.right;
 			}
 		}
-		lExcludedNodes += 2 * lParents[lParents.length - 1].z;
 
-		// Find the right-most end-node at `level += 1` and calculate the number of
-		// end-nodes that would have been to the right of this node (on its level);
-		// `rExcludedNodes`.
-		while (rNodeLevel < level + 1) {
-			if (rNode.right && rNode.left) {
-				// Current node has two children.
-				rParents.push(new OrderedTriple(lNode, level, rExcludedNodes));
+		// Move `rNode` to the next right-most node in `level += 1`;
+		// *************************************************************************
+		rNodeLevel = level;
+		while (lNode != rNode && rNodeLevel < level + 1) {
+			// When `rNode` has both children.
+			if (hasBothChildren(rNode)) {
+				rParents.push(new TreeNodeData(rNode, rNodeLevel, rExcludedNodes));
 				rNode = rNode.right;
+				rNodeLevel += 1;
 				rExcludedNodes *= 2;
-			} else if (rNode.right) {
-				// Current node has only right child.
+			}
+			// When `rNode` has only the right child.
+			else if (rNode.right) {
 				rNode = rNode.right;
+				rNodeLevel += 1;
 				rExcludedNodes *= 2;
-			} else if (lNode.left) {
-				// Current node has only left child.
+			}
+			// When `rNode` has only the left child.
+			else if (rNode.left) {
 				rNode = rNode.left;
+				rNodeLevel += 1;
 				rExcludedNodes = 2 * rExcludedNodes + 1;
-			} else {
-				// Backtrack to last ancestor node with children untraveresed and begin
-				// left-biased traversal to find the next left-most node of `level += 1`.
-				rNodeLevel = rParents[rParents.length - 1].y;
-				rNode = rParents.pop().x.right;
-				rExcludedNodes = 1;
+			}
+			// When `lNode` has no children.
+			else {
+				// Set `rNode` to the last ancestor node with an untraversed child and
+				// `rNodeLevel` to the level of that node.
+				rNodeLevel = rParents.at(-1).level;
+				rExcludedNodes = rParents.at(-1).excludedNodes;
+				rNode = rParents.pop().node.left;
 			}
 		}
-		rExcludedNodes += 2 * rParents[rParents.length - 1].z;
-
-		// Assign next max width.
-		maxWidth = Math.max(
-			maxWidth,
-			maxWidthOfLevel((level += 1)) - (lExcludedNodes + rExcludedNodes)
-		);
+		// Both `lNode` and `rNode` reached the next nodes desired in `level += 1`,
+		// so set the current level to `level += 1`.
+		// if (lNode !== rNode) {
+		if (lNode !== rNode || lNode.left || lNode.right) {
+			level += 1;
+			if (level == 4) console.log(lExcludedNodes + ' ' + rExcludedNodes);
+			mWidth = Math.max(
+				mWidth,
+				maxWidthOfLevel(level, lExcludedNodes, rExcludedNodes)
+			);
+		}
+		// }
 	}
-	return maxWidth;
+
+	return mWidth;
+
+	// ************************
+
+	function maxWidthOfLevel(
+		level: number,
+		lExcludedNodes: number,
+		rExcludedNodes: number
+	): number {
+		if (
+			!(level >= 0 && Number.isInteger(level)) ||
+			!(lExcludedNodes >= 0 && Number.isInteger(lExcludedNodes)) ||
+			!(rExcludedNodes >= 0 && Number.isInteger(rExcludedNodes))
+		)
+			throw Error();
+		return Math.pow(2, level - 1) - (lExcludedNodes + rExcludedNodes);
+	}
+
+	/** Checks if a tree node has 2 children. */
+	function hasBothChildren(tNode: TreeNode | null): boolean {
+		if (tNode && tNode.left && tNode.right) return true;
+		return false;
+	}
 }

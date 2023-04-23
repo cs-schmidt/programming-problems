989b78755e014a0765b52670cfeb7938d4e01222
cs-schmidt <es.schmidt.cs@gmail.com>
2023-04-23 16:59:57 -0600

    completed problem

---
+++ leetcode/leetcode-75/2023-04-23_leet75-121.ts
@@ -0,0 +1,32 @@
+// Problem 121: Best Time to Buy and Sell a Stock
+// NOTE: Popular question
+
+/**
+ * Constraints:
+ *
+ * 1) 1 <= prices.length <= 10^5
+ * 2) 0 <= prices[i] <= 10^4
+ *
+ */
+
+// *** First Version ***
+function maxProfit(prices: number[]): number {
+  // Initialize two variables `minEntry` and `profit`, where the former tracks 
+  // the minimum entry price encountered as we traverse `prices`, and the latter
+  // tracks the maximum profit. 
+  let minEntry: number = prices[0]; // set `minEntry` to the first price in `prices`.
+  let profit: number = 0; // set `profit` to 0 (handles case where `prices.length == 1`).
+
+  // Starting from the second index in `prices`, traverse the array and 
+  // update the `minEntry` and `profit` variables.
+  for (let i: number = 1; i < prices.length; i++) {
+    // When the `minEntry` is less than the current price being observed, then 
+    // update it to that price.
+    if (minEntry > prices[i]) minEntry = prices[i];
+    // When `minEntry` is less than or equal to the current price, check if the 
+    // difference between the current price and `minEntry` is a new max profit.
+    else if (prices[i] - minEntry > profit) profit = prices[i] - minEntry;
+  }
+
+  return profit;
+}

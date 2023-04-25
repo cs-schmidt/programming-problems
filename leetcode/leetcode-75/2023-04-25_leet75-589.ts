47a4a4e1a6de11f73dc271272d6ac3691925831f
cs-schmidt <ebs.euler@gmail.com>
2023-04-25 16:23:46 -0600

    feat: complete leetcode problem 589

---
+++ leetcode/leetcode-75/2023-04-25_leet75-589.ts
@@ -0,0 +1,34 @@
+// Problem 589: N-ary Preorder Traversal
+// NOTE: Popular question.
+// TODO: Produce further optimized version.
+
+/**
+ * Constraints:
+ *
+ * 1) The number of nodes in the tree is in the range [0, 10^4].
+ * 2) 0 <= Node.val <= 10^4
+ * 3) The height of the n-ary tree is less than or equal to 1000
+ *
+ */
+
+// Definition for node.
+class Node {
+  val: number;
+  children: Node[];
+  constructor(val?: number) {
+    this.val = val === undefined ? 0 : val;
+    this.children = [];
+  }
+}
+
+/* 
+   Solutions:
+   ========================================================================== */
+
+// Solution 1: recursive algorithm
+function preorder(root: Node | null): number[] {
+  if (!root) return [];
+  return [root.val].concat(
+    ...root.children.map((node: Node) => preorder(node))
+  );
+}

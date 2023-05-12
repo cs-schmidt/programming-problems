7692d6689bf56730a11ef29fbea7ad8df63b3d29
cs-schmidt <ebs.euler@gmail.com>
2023-05-12 17:13:17 -0600

    feat: submit new solution for problem 205

--- leetcode/leetcode-75/2023-04-21_leet75-205.ts
+++ leetcode/leetcode-75/2023-04-21_leet75-205.ts
@@ -1,37 +1,39 @@
 // Problem 205: Isomorphic Strings
-// NOTE: Popular question.
-// TODO: Produce further optimized version.
+
+/**
+ * Constraints:
+ *
+ * 1) 1 <= s.length <= 5 * 10^4
+ * 2) t.length == s.length
+ * 3) s and t consist of any valid ascii character.
+ */
 
 function isIsomorphic(s: string, t: string): boolean {
-	// If strings `s` and `t` are isomorphic, then they must have the same length.
-	if (s.length !== t.length) return false;
+  /**
+   * Given the constraints of the problem, we only have to determine if there's a
+   * bijective mapping between the elements of `s` and `t`. We need to memorize
+   * the mappings we've encountered in as we progress through the strings
+   * comparing corresponding elements:
+   *
+   * 1) `mappings`: an object which stores the previous mappings encountered from
+   *    `s` to `t`.
+   * 2) `mappedTo`: a set of the elements from `t` that have already been mapped
+   *    to.
+   */
+  const mappings: { [key: string]: string } = {};
+  const mappedTo: Set<string> = new Set();
 
-  // Initialize two data structures: an object of character mappings from `s` to 
-  // `t`, and the set of unique characters in `t` that have already been mapped.
-	const charMappings: Object = {};
-	const mappedChars: Set<String> = new Set();
+  // Progress through the strings comparing correspondng elements. We return
+  // `false` as soon as we know a bijective mapping cannot exist.
+  for (let i = 0; i < s.length; i++) {
+    // Check the conditions for a bijective mapping to exist.
+    if (mappings[s[i]] && mappings[s[i]] != t[i]) return false;
+    if (mappedTo.has(t[i]) && mappings[s[i]] != t[i]) return false;
 
-  // Iterate over each index shared by the strings.
-	for (let i: number = 0; i < s.length; i++) {
-		// When both `s[i]` and `t[i]` haven't been mapped.
-		if (!charMappings[s[i]] && !mappedChars.has(t[i])) {
-			// Store the mapping of `s[i]` to `t[i]` in `charMappings`, and the fact 
-      // that `t[i]` has been mapped in `mappedChars`.
-			charMappings[s[i]] = t[i];
-			mappedChars.add(t[i]);
-		}
-		// When `s[i]` hasn't been mapped, but `t[i]` has been mapped, then we know
-    // that `s` and `t` are not isomorphic.
-		else if (!charMappings[s[i]] && mappedChars.has(t[i])) return false;
-		// When `s[i]` has been mapped, but `t[i]` hasn't been mapped, then we know
-    // that `s` and `t` are not isomorphic.
-		else if (charMappings[s[i]] && !mappedChars.has(t[i])) return false;
-		// When `s[i]` is already been mapped and `t[i]` is already mapped, but not
-		// to each other.
-		else if (charMappings[s[i]] !== t[i]) return false;
-    // When none of the above conditions hold, we know `s[i]` and `t[i]` are 
-    // already mapped to one another. Thus we continue to the next iteration.
-	}
+    // Log the mapping information.
+    mappings[s[i]] = t[i];
+    mappedTo.add(t[i]);
+  }
 
-	return true;
+  return true;
 }

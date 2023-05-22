780871aeaa3b27e42eccca6c250cc91c489f44d0
cs-schmidt <ebs.euler@gmail.com>
2023-05-21 23:41:54 -0600

    feat: complete leetcode problem 1071

--- leetcode/leetcode-75/2023-05-20_leet75-1071.ts
+++ leetcode/leetcode-75/2023-05-20_leet75-1071.ts
@@ -1,5 +1,4 @@
 // Problem 1071: Greatest Common Divisor of Strings
-// TODO: Complete this challenge problem.
 
 /**
  * Constraints:
@@ -9,58 +8,15 @@
  *
  */
 
+// TODO: Improve solution.
 function gcdOfStrings(str1: string, str2: string): string {
-  // Start by computing the GCD between `str1.length` and `str2.length`. The
-  // longest substring dividing both `str1` and `str2` can't be longer than
-  // this value.
-  var maxResultLen = gcd(str1.length, str2.length);
-
-  // Now, we want to check for the longest repeating substring that occurs in
-  // `str1` and `str2` up to `maxResultLen`.
-  var maxSubStr: string = '';
-  var subStrIdx: number = 0;
-  var searchIdx: number = 0;
-  while (searchIdx < Math.min(str1.length, str2.length)) {
-    if (str1[searchIdx] != str2[searchIdx]) return '';
-    while (
-      maxSubStr[subStrIdx] == str1[searchIdx] &&
-      subStrIdx != maxSubStr.length
-    ) {
-      subStrIdx += 1;
-      searchIdx += 1;
+  if (str1 + str2 == str2 + str1) {
+    while (str1 != str2) {
+      if (str1.length > str2.length)
+        str1 = str1.slice(str2.length, str1.length);
+      else str2 = str2.slice(str1.length, str2.length);
     }
-    if (subStrIdx < maxSubStr.length || maxSubStr.length == 0) {
-      if (searchIdx + 1 > maxResultLen) return '';
-      maxSubStr = str1.slice(0, searchIdx + 1);
-    }
-    if (subStrIdx != maxSubStr.length) searchIdx += 1;
-    subStrIdx = 0;
+    return str1;
   }
-
-  // Now, we want to check the remainder of the longer string (either `str1` or
-  // `str2`).
-  var longerStr = str1.length > str2.length ? str1 : str2;
-  searchIdx = Math.min(str1.length, str2.length);
-  while (searchIdx < longerStr.length) {
-    while (subStrIdx < maxSubStr.length) {
-      if (maxSubStr[subStrIdx] != longerStr[searchIdx]) return '';
-      subStrIdx += 1;
-      searchIdx += 1;
-    }
-    if (subStrIdx != maxSubStr.length) searchIdx += 1;
-    subStrIdx = 0;
-  }
-
-  return maxSubStr;
-}
-
-/**
- * Computes the greatest common divisor between two numbers.
- */
-function gcd(num1: number, num2: number) {
-  while (num1 != num2) {
-    if (num1 > num2) num1 -= num2;
-    else num2 -= num1;
-  }
-  return num1;
+  return '';
 }

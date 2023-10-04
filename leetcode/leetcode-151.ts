c5851626ac171c81377129ad5ec0a2e94aa7c479
cs-schmidt <ebs.euler@gmail.com>
2023-10-04 14:28:28 -0600

    feat: complete leetcode problem 151

---
+++ leetcode/leetcode-151.ts
@@ -0,0 +1,14 @@
+// Problem 151: Reverse Words in a String
+
+/**
+ * Constraints:
+ *
+ * 1) 1 <= s.length <= 10^4
+ * 2) s contains English letters (upper-case and lower-case), digits, and
+ *    spaces.
+ * 3) There is at least one word in s.
+ */
+
+function reverseWords(str: string): string {
+  return str.trim().split(/\s+/).reverse().join(' ');
+};

e6e287a1b112028aec50e0efe86016540c5102ce
cs-schmidt <ebs.euler@gmail.com>
2023-10-27 18:53:55 -0600

    feat: checkpoint leetcode problem 53

--- leetcode/leetcode-53.ts
+++ leetcode/leetcode-53.ts
@@ -5,9 +5,20 @@
  *
  * 1) 1 <= nums.length <= 10^5
  * 2)  -10^4 <= nums[i] <= 10^4
- *
  */
 
 function maxSubArray(nums: number[]): number {
-  return 0;
+  let pivotIndex = 0;
+  let result = nums[pivotIndex];
+
+  while (pivotIndex < nums.length) {
+    let sum = 0;
+    for (let i = pivotIndex; i < nums.length; i++) {
+      sum += nums[i];
+      if (sum > result) result = sum;
+    }
+    pivotIndex += 1;
+  }
+
+  return result;
 }

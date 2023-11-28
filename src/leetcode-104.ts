1baf7c74c8b5efe76bd0debbcad98a4f4f18b557
cs-schmidt <ebs.euler@gmail.com>
2023-11-27 17:14:49 -0700

    feat: complete leetcode problem 104

--- src/leetcode-104.ts
+++ src/leetcode-104.ts
@@ -1,49 +1,57 @@
-// Problem 104: Maximum Depth of Binary Tree
-
 /**
- * Constraints
+ * Problem 104: Maximum Depth of Binary Tree
  *
- * 1) The number of nodes in the tree is in the range [0, 10^4].
- * 2) -100 <= Node.val <= 100
+ * Constraints
+ *  1. The number of nodes in the tree is in the range [0, 10^4].
+ *  2. -100 <= Node.val <= 100
  */
 
-// Definition for a binary tree node.
-interface TreeNode {
+/** Definition of a binary tree node. */
+class TreeNode {
   val: number;
   left: TreeNode | null;
   right: TreeNode | null;
+
+  constructor(
+    val: number = 0,
+    left: TreeNode | null = null,
+    right: TreeNode | null = null
+  ) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
 }
 
+/**
+ * Recursive Approach.
+ *
+ * This solution is adapted from a recursive depth-first search (DFS) algorithm.
+ * We define an internal procedure `recursivePostTraversal()` and a variable
+ * `traversalDepth`, then we use the `recursivePostTraversal()` to do a DFS
+ * which adjusts `traversalDepth`. The value of `traversalDepth` then compared
+ * with the greatest `result` found so far and updated. At the end of things
+ * `result` is returned.
+ *
+ * Complexity: O(nodes) time and O(height) + O(1) auxiliary space.
+ */
 function maxDepth(root: TreeNode | null): number {
-  const path: TreeNode[] = root ? [root] : [];
-  const markedAncestors: Set<TreeNode> = new Set();
-  let result: number = path.length;
+  if (!root) return 0;
 
-  const iterations = 6;
-  let count = 1;
-
-  while (path.length != 0 && count < iterations) {
-    // Continually go down the left children and add them to `path`.
-    while (path.at(-1).left) path.push(path.at(-1).left);
-
-    // The current node (the last element of `path`) has no left child, so we
-    // check for a right child.
-    if (path.at(-1).right) {
-      markedAncestors.add(path.at(-1));
-      path.push(path.at(-1).right);
-    }
-    // There's no left or right child, so were at a leaf node.
-    else {
-      if (path.length > result) result = path.length;
-      path.pop();
-      while (markedAncestors.has(path.at(-1))) {
-        markedAncestors.delete(path.at(-1));
-        path.pop();
-      }
-    }
-
-    count += 1;
-  }
+  var result = 0;
+  var traversalDepth = 0;
+  recursivePostTraversal(root);
 
   return result;
+
+  // Internal Procedures
+  // =================================================================
+
+  function recursivePostTraversal(node: TreeNode | null) {
+    traversalDepth += 1;
+    if (node.left) recursivePostTraversal(node.left);
+    if (node.right) recursivePostTraversal(node.right);
+    if (traversalDepth > result) result = traversalDepth;
+    traversalDepth -= 1;
+  }
 }

f98955a30972ed2f35b24f4290c27d4e1fd56163
cs-schmidt <ebs.euler@gmail.com>
2023-12-05 22:28:13 -0700

    feat: complete leetcode problem 1207

---
+++ src/leetcode-1207.ts
@@ -0,0 +1,25 @@
+/**
+ * Problem 1207: Unique Number of Occurences
+ *
+ * Constraints:
+ *  1. 1 <= arr.length <= 1000
+ *  2. -1000 <= arr[i] <= 1000
+ */
+
+/**
+ * Iterative and Imperative Solution
+ *
+ * Complexity: O(n) time and O(n) auxiliary space.
+ */
+function uniqueOccurrences(arr: number[]): boolean {
+  const elementFrequencies: Map<number, number> = new Map();
+  const distictFrequencies: Set<number> = new Set();
+
+  for (const element of arr)
+    elementFrequencies.set(element, elementFrequencies.get(element) + 1 || 1);
+
+  for (const frequency of elementFrequencies.values())
+    distictFrequencies.add(frequency);
+
+  return elementFrequencies.size === distictFrequencies.size;
+}

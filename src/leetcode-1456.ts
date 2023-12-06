e202997b247c0f4e2b127ba4d57ef07a0d198768
cs-schmidt <ebs.euler@gmail.com>
2023-12-05 23:36:44 -0700

    feat: complete leetcode problem 1456

---
+++ src/leetcode-1456.ts
@@ -0,0 +1,36 @@
+/**
+ * Problem 1456: Maximum Number of Vowels in a Substring of Given Length
+ *
+ * Constraints:
+ *  1. 1 <= s.length <= 10^5
+ *  2. s consists of lowercase English letters.
+ *  3. 1 <= k <= s.length
+ */
+
+/**
+ * Iterative and Imperative Solution
+ *
+ * Complexity: O(n) time and O(1) auxiliary space.
+ */
+function maxVowels(s: string, k: number): number {
+  const vowelRegex = /^[aeiou]$/;
+  let maxSubStrCount = 0;
+  let currSubStrCount = 0;
+
+  // Initialize `maxSubStrCount` and `currSubStrCount`.
+  for (let i = 0; i <= k - 1; i++) {
+    if (vowelRegex.test(s[i])) maxSubStrCount += 1;
+  }
+  currSubStrCount = maxSubStrCount;
+
+  // Iteratively slide the substring window, determine the amount of vowels in
+  // that substring, and update `maxSubStrCount` if it's greater.
+  for (let step = k; step < s.length; step++) {
+    if (maxSubStrCount === k) return maxSubStrCount;
+    if (vowelRegex.test(s[step - k])) currSubStrCount -= 1;
+    if (vowelRegex.test(s[step])) currSubStrCount += 1;
+    if (currSubStrCount > maxSubStrCount) maxSubStrCount = currSubStrCount;
+  }
+
+  return maxSubStrCount;
+}

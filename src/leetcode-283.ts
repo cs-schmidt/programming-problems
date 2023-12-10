ca674b16607448f225924a91c5d19f5e235f00f6
cs-schmidt <ebs.euler@gmail.com>
2023-12-10 03:14:50 -0700

    feat: complete leetcode problem 283

--- src/leetcode-283.ts
+++ src/leetcode-283.ts
@@ -5,11 +5,8 @@
  *  1. 1 <= nums.length <= 10^4
  *  2. -2^31 <= nums[i] <= 2^31 - 1
  *  3. Must modify `nums` in-place without making a copy of it.
+ *  4. The sortition must be stable.
  */
-
-// It's a sortition problem.
-
-// NOTE: We need to minimize the number of operations done.
 
 /**
  * Iterative and Imperative Solution.
@@ -17,5 +14,25 @@
  * Complexity: O(n) time and O(1) auxiliary space.
  */
 function moveZeroes(nums: number[]): void {
-  // TODO: Implement this.
+  let zeroIndex = nums.indexOf(0);
+  let nonZeroIndex = zeroIndex + 1;
+
+  while (nums[nonZeroIndex] === 0 && nonZeroIndex < nums.length)
+    nonZeroIndex += 1;
+
+  if (zeroIndex === -1 || nonZeroIndex === nums.length) return;
+
+  while (nums[zeroIndex] === 0 && nonZeroIndex < nums.length) {
+    const nonZeroVal = nums[nonZeroIndex];
+
+    nums[nonZeroIndex] = 0;
+    nums[zeroIndex] = nonZeroVal;
+
+    while (nums[zeroIndex] !== 0 && zeroIndex < nums.length) zeroIndex += 1;
+
+    nonZeroIndex = zeroIndex + 1;
+
+    while (nums[nonZeroIndex] === 0 && nonZeroIndex < nums.length)
+      nonZeroIndex += 1;
+  }
 }

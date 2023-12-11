cef88ae7a4ca23482a3ff70045fdb05499ea7a49
cs-schmidt <ebs.euler@gmail.com>
2023-12-11 00:54:54 -0700

    feat: checkpoint leetcode problem 338

---
+++ src/leetcode-338.py
@@ -0,0 +1,21 @@
+"""
+Problem 338: Counting Bits
+
+Constraints:
+ 1. 0 <= n <= 10^5
+"""
+
+class Solution:
+    def countBits(self, n: int) -> list[int]:
+        """
+        <solution type>
+
+        Complexity: <time complexity> and <space complexity>.
+        """
+        oneBitsForIndexes: list[int] = []
+
+        for i in range(n + 1):
+            #
+            x
+
+        return oneBitsForIndexes

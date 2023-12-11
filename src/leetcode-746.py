8bef3f8ed5ed80d4d96047bfec573f0bf274f2ce
cs-schmidt <ebs.euler@gmail.com>
2023-12-10 22:00:48 -0700

    feat: checkpoint leetcode problem 746

---
+++ src/leetcode-746.py
@@ -0,0 +1,17 @@
+"""
+Problem 746: Min Cost Climbing Stairs
+
+Constraints:
+ 1. 2 <= cost.length <= 1000
+ 2. 0 <= cost[i] <= 999
+"""
+
+class Solution:
+    def minCostClimbingStairs(self, stepCosts: list[int]) -> int:
+        """
+        <solution type>
+
+        Complexity: <time complexity> and <space complexity>.
+        """
+
+        return 0

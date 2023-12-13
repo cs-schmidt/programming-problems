d6f54e4c3434cd4ab3786cf0a6e75dc126104b5c
cs-schmidt <ebs.euler@gmail.com>
2023-12-13 01:39:47 -0700

    feat: checkpoint leetcode problem 216

--- src/leetcode-216.ts
+++ src/leetcode-216.ts
@@ -12,51 +12,41 @@
  * Complexity: <time complexity> and <space complexity>.
  */
 function combinationSum3(k: number, n: number): number[][] {
-  if (k > n || k < minSelections(n)) return [];
-  const partitions: number[][] = [];
-  let nextMax = n <= 9 ? n : 9;
-  let remainder = n;
-  let selections = k;
-  const combination: number[] = [];
-  while (true /* something */) {
-    remainder -= nextMax;
-    if (remainder > 0) {
-      combination.push(nextMax);
-      selections -= 1;
-    }
-    nextMax -= 1;
+  const result: number[][] = [];
+  let partition: number[] = [];
+  let max = 9;
+  let c = k;
+
+  // Condition indicates if a selection of `k` distinct integers between 1 and
+  // `max` can sum together and equate to `n`.
+  while (naturalSum(c) <= n && n <= c * max + naturalSum(c - 1)) {
+    partition.push(max);
+    n -= max;
+    c -= 1;
+    max -= 1;
   }
-  return partitions;
+
+  if (partition.length === k) {
+    result.push(partition);
+    partition = [];
+  }
+  // Otherwise.
+  else {
+    //
+  }
+
+  return result;
 
   // Internal Procedures
   // =================================================================
   /**
    * Computes the sum of positive integers up to the input.
    * @param {number} num - A natural number (a positive integer).
-   * @returns {number} The sum of positive integers up to the input or NaN if it
-   *   can't be computed.
+   * @return {number} The sum of positive integers up to the input, or NaN if it
+   *   can't be computed for the given input.
    */
   function naturalSum(num: number) {
     if (num <= 0 || !Number.isInteger(num)) return NaN;
     return (num * (num + 1)) / 2;
-  }
-
-  /**
-   * Computes the minimum number of selections from {1, 2, ..., 9} who's sum can
-   * represent the input.
-   * @param {number} num - A positive integer.
-   * @returns {number} The minimum number of selections.
-   */
-  function minSelections(num: number): number {
-    if (num <= 0 || num > 45 || !Number.isInteger(num)) return NaN;
-    if (num === 45) return 9;
-    if (num >= 43 && num <= 44) return 8;
-    if (num >= 40 && num <= 42) return 7;
-    if (num >= 36 && num <= 39) return 6;
-    if (num >= 31 && num <= 35) return 5;
-    if (num >= 25 && num <= 30) return 4;
-    if (num >= 18 && num <= 24) return 3;
-    if (num >= 10 && num <= 17) return 2;
-    return 1;
   }
 }

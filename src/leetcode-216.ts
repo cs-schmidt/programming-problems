f13574414a067b5df160aa7df4c58506009a921b
cs-schmidt <ebs.euler@gmail.com>
2023-12-15 00:36:16 -0700

    feat: checkpoint leetcode problem 216

--- src/leetcode-216.ts
+++ src/leetcode-216.ts
@@ -14,41 +14,71 @@
  * Complexity: <time complexity> and <space complexity>.
  */
 function combinationSum3(k: number, n: number): number[][] {
-  const result: number[][] = [];
-  let partition: number[] = [];
   let max = 9;
-  let c = k;
+  const partition: number[] = [];
+  const result: number[][] = [];
 
-  // Condition indicates if a selection of `k` distinct integers between 1 and
-  // `max` can sum together and equate to `n`.
-  while (naturalSum(c) <= n && n <= c * max + naturalSum(c - 1)) {
+  // XXX: Does it stop when partition is full?
+  while (naturalSum(k) <= n && n >= k * max - naturalSum(k - 1)) {
     partition.push(max);
-    n -= max;
-    c -= 1;
     max -= 1;
-  }
-
-  if (partition.length === k) {
-    result.push(partition);
-    partition = [];
-  }
-  // Otherwise.
-  else {
-    //
+    k -= 1;
   }
 
   return result;
-
-  // Internal Procedures
-  // =================================================================
-  /**
-   * Computes the sum of positive integers up to the input.
-   * @param {number} num - A natural number (a positive integer).
-   * @return {number} The sum of positive integers up to the input, or NaN if it
-   *   can't be computed for the given input.
-   */
-  function naturalSum(num: number) {
-    if (num <= 0 || !Number.isInteger(num)) return NaN;
-    return (num * (num + 1)) / 2;
-  }
 }
+
+// Additional Procedures
+// =================================================================
+/**
+ * Computes the sum of positive integers up to and including the input.
+ * @param {number} num - A positive integer (i.e., a natural number).
+ * @return {number} A positive integer or NaN if the sum can't be computed.
+ */
+function naturalSum(num: number): number {
+  return !Number.isInteger(num) ||
+    num < 1 ||
+    Math.abs(num) > Number.MAX_SAFE_INTEGER
+    ? NaN
+    : (num * (num / 2)) / 2;
+}
+
+// function combinationSum3(k: number, n: number): number[][] {
+//   const result: number[][] = [];
+//   let partition: number[] = [];
+//   let max = 9;
+//   let c = k;
+
+//   // Condition indicates if a selection of `k` distinct integers between 1 and
+//   // `max` can sum together and equate to `n`.
+//   while (naturalSum(c) <= n && n <= c * max + naturalSum(c - 1)) {
+//     partition.push(max);
+//     n -= max;
+//     c -= 1;
+//     max -= 1;
+//   }
+
+//   if (partition.length === k) {
+//     result.push(partition);
+//     partition = [];
+//   }
+//   // Otherwise.
+//   else {
+//     //
+//   }
+
+//   return result;
+
+//   // Internal Procedures
+//   // =================================================================
+//   /**
+//    * Computes the sum of positive integers up to the input.
+//    * @param {number} num - A natural number (a positive integer).
+//    * @return {number} The sum of positive integers up to the input, or NaN if it
+//    *   can't be computed for the given input.
+//    */
+//   function naturalSum(num: number) {
+//     if (num <= 0 || !Number.isInteger(num)) return NaN;
+//     return (num * (num + 1)) / 2;
+//   }
+// }

8ac10d44f7fd7e12c6e5729d21bbc873d997c83d
cs-schmidt <ebs.euler@gmail.com>
2023-12-20 00:39:43 -0700

    feat: checkpoint leetcode problem 2336

---
+++ src/leetcode-2336.py
@@ -0,0 +1,26 @@
+"""
+Problem 2336: Smallest Number in Infinite Set
+
+Constraints:
+ 1. 1 <= num <= 1000
+ 2. At most 1000 calls will be made in total to popSmallest and addBack.
+"""
+
+class SmallestInfiniteSet:
+    """
+    Contains all the positive integers.
+    """
+
+    def __init__(self):
+
+        
+    def popSmallest(self) -> int:
+        """
+        Removes and returns the smallest integer contained in the infinite set.
+        """
+        
+    def addBack(self, num: int) -> None:
+        """
+        Adds a positive integer num back into the infinite set, if it is not
+        already in the infinite set.
+        """

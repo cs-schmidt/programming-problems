70252d9952e22d1d420f7e8117d0253f9b4513e1
cs-schmidt <ebs.euler@gmail.com>
2023-12-19 19:55:18 -0700

    feat: complete leetcode problem 1318

--- src/leetcode-1318.js
+++ src/leetcode-1318.js
@@ -7,15 +7,28 @@
  *  3. 1 <= c <= 10^9
  */
 
-// TODO: Complete this problem.
+/**
+ * Iterative and Imperative Solution
+ *
+ * Complexity: O(32) time and O(1) auxiliary space.
+ */
 
 /**
- * <solution type>
- *
- * Complexity: <time complexity> and <space complexity>.
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
  */
 function minFlips(a, b, c) {
-  var result = 0;
+  let result = 0;
+  for (let i = 31; i >= 0; i--) {
+    const bit1 = (a >> i) & 1;
+    const bit2 = (b >> i) & 1;
+    const bit3 = (c >> i) & 1;
+
+    if ((bit1 || bit2) && !bit3) result += bit1 && bit2 ? 2 : 1;
+    else if (!(bit1 || bit2) && bit3) result += 1;
+  }
 
   return result;
 }

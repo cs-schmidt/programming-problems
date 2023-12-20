94bcf9faf086bea75d1f0f8f835777077e925703
cs-schmidt <ebs.euler@gmail.com>
2023-12-19 18:34:19 -0700

    feat: checkpoint leetcode problem 1318

---
+++ src/leetcode-1318.js
@@ -0,0 +1,21 @@
+/**
+ * Problem 1318: Minimum Flips to Make a OR b Equal to c
+ *
+ * Constraints:
+ *  1. 1 <= a <= 10^9
+ *  2. 1 <= b <= 10^9
+ *  3. 1 <= c <= 10^9
+ */
+
+// TODO: Complete this problem.
+
+/**
+ * <solution type>
+ *
+ * Complexity: <time complexity> and <space complexity>.
+ */
+function minFlips(a, b, c) {
+  var result = 0;
+
+  return result;
+}
---
+++ src/leetcode-215.py
@@ -0,0 +1,20 @@
+"""
+Problem 215: Kth Largest Element in an Array
+
+Constraints:
+ 1. 1 <= k <= nums.length <= 10^5
+ 2. -10^4 <= nums[i] <= 10^4
+"""
+
+# TODO: Complete this problem.
+
+class Solution:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        """
+        <solution type>
+
+        Complexity: <time complexity> and <space complexity>.
+        """
+        result = 0
+
+        return result

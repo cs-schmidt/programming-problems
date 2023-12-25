931d5387d92010438a248790886b1803a7255581
cs-schmidt <ebs.euler@gmail.com>
2023-12-24 19:06:39 -0700

    feat: update leetcode problem 63

--- src/leetcode-63.ts
+++ src/leetcode-63.ts
@@ -6,24 +6,33 @@
  */
 
 /**
- * Dynamic Programming Soltuion
+ * Iterative and Imperative Solution
  *
- * Complexity: <time complexity> and <space complexity>.
+ * The solution exploits the fact that the number of unique paths from <0,0> to
+ * <i,j> on a 2D grid, is equal to all the unique orderings of i down moves and
+ * j right moves. It turns out that we can use the formula for the permutations
+ * of a multiset to compute the answer. Thus, we return the result of this
+ * formula where <rows,cols> is the endpoint.
+ *
+ * Complexity: O(rows + cols) time and O(max(rows, cols)) auxiliary space.
  */
 function uniquePaths(rows: number, cols: number): number {
   return (
-    factorial(rows + cols - 2) / (factorial(rows - 1) * factorial(cols - 1))
+    factorial(rows - 1 + (cols - 1)) /
+    (factorial(rows - 1) * factorial(cols - 1))
   );
 }
 
+/**
+ * Computes the factorial of a positive integer num.
+ *
+ * - A linear recursion process.
+ * - Complexity: O(n) time and O(n) space.
+ *
+ * @param {number} num - A positive integer.
+ * @returns {number}
+ */
 function factorial(num: number): number {
-  if (num === 0) return 1;
-  if (num === 1) return 1;
+  if (num <= 1) return 1;
   return num * factorial(num - 1);
 }
-
-// - Given points <i,j> and <0,0> on a 2D grid, any path from <0,0> to <i,j>
-//   must consist of an i number of moves down and a j number of moves to the
-//   right.
-// - The number of paths from <0,0> to <i,j> on a 2D grid is equal to all the
-//   unique orderings of i down moves and j right moves.

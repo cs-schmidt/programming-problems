258248fbbf307a5483a71dbfe37002ffe4cd9495
cs-schmidt <ebs.euler@gmail.com>
2023-12-24 19:18:05 -0700

    feat: checkpoint leetcode problem 435

---
+++ src/leetcode-435.js
@@ -0,0 +1,28 @@
+/**
+ * Problem 435: Non-overlapping Intervals
+ *
+ * Constraints:
+ *  1. 1 <= intervals.length <= 10^5
+ *  2. intervals[i].length == 2
+ *  3. -5 * 10^4 <= starti < endi <= 5 * 10^4
+ */
+
+/**
+ * Iterative and Imperative Solution
+ *
+ * Complexity: <time complexity> and <space complexity>.
+ */
+
+/**
+ * Calculates the minimum number of intervals must be removed to get a list on
+ * non-overlapping intervals.
+ * @param {number[][]} intervals - An array of intervals.
+ * @return {number}
+ */
+function eraseOverlapIntervals(intervals) {
+  const result = 0;
+
+  // Update `result` to the number of min removals from `intervals`.
+
+  return result;
+}

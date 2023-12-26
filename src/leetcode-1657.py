793bb105c7403420a0adcdededa2e9a3fdfe9f22
cs-schmidt <ebs.euler@gmail.com>
2023-12-26 12:50:04 -0700

    feat: complete leetcode problem 1657

--- src/leetcode-1657.py
+++ src/leetcode-1657.py
@@ -12,28 +12,32 @@ class Solution:
         """
         Iterative and Imperative Solution
 
-        Complexity: O(n*log(n)) time and O(n) auxilary space (where n is equal
-                    to `len(word1)` and `len(word2)`).
+        Operations 1 and 2 cannot change the set of distinct characters that
+        comprise a word, nor can they change the set of multiplicities for
+        distinct characters in  a string: that's the invariant. So, we write an
+        algorithm that checks two things: if the words are comprised of the same
+        set of distinct characters, and if the multiplicities across their
+        distinct characters aligns.
+
+        Complexity: O(n * log(n)) time and O(n) auxiliary space; where n
+                    represents the length of word1 and word2. Our analysis
+                    assumes that the lengths are the same.
         """
-        # Regardless of operation 1 and operation 2, a string will always
-        # maintain the same number of distinct characters and multiplicities
-        # across each: that's the invariant here.
+        if len(word1) != len(word2): return False
 
-        if len(word1) != len(word2):
-            return False
+        charFrequencies1: dict[int] = {}
+        charFrequencies2: dict[int] = {}
+        for char1, char2 in zip(word1, word2):
+            charFrequencies1[char1] = charFrequencies1.get(char1, 0) + 1
+            charFrequencies2[char2] = charFrequencies2.get(char2, 0) + 1
+        
+        if len(set(charFrequencies1.keys()).difference(set(charFrequencies2.keys()))): return False
 
-        word1CharFrequencies: dict = {}
-        word2CharFrequencies: dict = {}
-        for i in range(len(word1)):
-            word1CharFrequencies[word1[i]] = word1CharFrequencies.get(word1[i], 0) + 1
-            word2CharFrequencies[word2[i]] = word2CharFrequencies.get(word2[i], 0) + 1
-
-        frequencies1: list[int] = list(word1CharFrequencies.values())
-        frequencies2: list[int] = list(word2CharFrequencies.values())
+        frequencies1: list[int] = list(charFrequencies1.values())
+        frequencies2: list[int] = list(charFrequencies2.values())
         frequencies1.sort()
         frequencies2.sort()
-
         for freq1, freq2 in zip(frequencies1, frequencies2):
-            if freq1 != freq2:
-                return False
+            if freq1 != freq2: return False
+
         return True

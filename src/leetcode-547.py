263d81fe8ae1a34c57fc6b7c7a1f0683e8badf9f
cs-schmidt <ebs.euler@gmail.com>
2023-12-31 16:04:15 -0700

    feat: checkpoint leetcode problem 547

---
+++ src/leetcode-547.py
@@ -0,0 +1,23 @@
+"""
+Problem 547: Number of Provinces
+
+Constraints:
+ 1. 1 <= n <= 200
+ 2. n == isConnected.length
+ 3. n == isConnected[i].length
+ 4. isConnected[i][j] is 1 or 0.
+ 5. isConnected[i][i] == 1
+ 6. isConnected[i][j] == isConnected[j][i]
+"""
+
+class Solution:
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+        """
+        <solution type>
+
+        Complexity: <time complexity> and <space complexity>.
+        """
+        result = 0
+
+           
+        return result

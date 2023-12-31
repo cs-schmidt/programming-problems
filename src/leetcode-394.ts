49b32afc7f677d14fb79e656a3fdd8dffa3cd704
cs-schmidt <ebs.euler@gmail.com>
2023-12-30 18:22:16 -0700

    feat: checkpoint leetcode problem 394

---
+++ src/leetcode-394.ts
@@ -0,0 +1,22 @@
+/**
+ * Problem 394: Decode String
+ *
+ * Constraints:
+ *  1. 1 <= s.length <= 30
+ *  2. s consists of lowercase English letters, digits, and square brackets '[]'.
+ *  3. s is guaranteed to be a valid input.
+ *  4. All the integers in s are in the range [1, 300].
+ */
+
+/**
+ * <solution type>
+ *
+ * Complexity: <time complexity> and <space complexity>.
+ */
+function decodeString(s: string): string {
+  const result = '';
+
+  // Build `result` by parsing the string `s`.
+
+  return result;
+}

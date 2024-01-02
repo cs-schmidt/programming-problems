b0c32e12f3dd425f28d64b61673a04ee51c6e10b
cs-schmidt <ebs.euler@gmail.com>
2024-01-01 18:57:17 -0700

    feat: complete leetcode problem 547

--- src/leetcode-547.py
+++ src/leetcode-547.py
@@ -10,22 +10,44 @@ Constraints:
  6. isConnected[i][j] == isConnected[j][i]
 """
 
+# Description
+# =================================================================
+# We want to find the amount of disjoint sets of (directly or
+# indirectly) connected cities: that's the number of provinces. And, the
+# information for which cities interconnect is encoded within the matrix
+# 'isConnected'.
+
+# 'isConnected' is an adjacency matrix.
+
 class Solution:
     def findCircleNum(self, isConnected: list[list[int]]) -> int:
         """
         Iterative and Imperative Solution
 
-        Complexity: <time complexity> and <space complexity>.
+        Complexity: O(n^2) time and O(n) auxiliary space.
         """
-        # Description
-        # ================================================================= 
-        # We want to find the amount of disjoint sets of (directly or
-        # indirectly) connected cities: that's the number of provinces. And, the
-        # information for which cities interconnect is encoded within the matrix
-        # 'isConnected'.
 
-        # The matrix 'isConnected' must be symmetric. 
+        result: int = 0
+        totalCities: int = len(isConnected)
+        citiesToVisit: list[int] = [0]
+        citiesVisited: set[int] = set()
 
-        result = 0
+        while len(citiesVisited) < totalCities:
+            while citiesToVisit:
+                city: int = citiesToVisit.pop()
+                citiesVisited.add(city)
+
+                connections: list[int] = isConnected[city]
+                for i in range(len(connections)):
+                    if connections[i] == 1 and i not in citiesVisited:
+                        citiesToVisit.append(i)
+
+            if len(citiesVisited) < totalCities:
+                citiesLeft: set[int] = set(
+                    [i for i in range(0, totalCities)]
+                ).difference(citiesVisited)
+                citiesToVisit.append(citiesLeft.pop())
+
+            result += 1
 
         return result

968e20b2d8e1e7f1f9c6a636be89b9eb0b0bbd8b
cs-schmidt <ebs.euler@gmail.com>
2024-01-02 17:16:45 -0700

    feat: checkpoint leetcode problem 714

---
+++ src/leetcode-714.js
@@ -0,0 +1,24 @@
+/**
+ * Problem 714: Best Time to Buy and Sell Stock with Transaction Fee
+ *
+ * Constraints:
+ *  1. 1 <= prices.length <= 5 * 10^4
+ *  2. 1 <= prices[i] < 5 * 10^4
+ *  3. 0 <= fee < 5 * 10^4
+ */
+
+/**
+ * <solution type>
+ *
+ * Complexity: <time complexity> and <space complexity>.
+ */
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+function maxProfit(prices, fee) {
+  const result = 0;
+
+  return result;
+}

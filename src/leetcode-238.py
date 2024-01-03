408fe8aad92e52c5b0533fb434100c83869baab3
cs-schmidt <ebs.euler@gmail.com>
2024-01-03 12:46:36 -0700

    feat: checkpoint leetcode problem 238

---
+++ src/leetcode-238.py
@@ -0,0 +1,20 @@
+"""
+Problem 238: Product of Array Except Self
+
+Constraints:
+ 1. 2 <= nums.length <= 10^5
+ 2. -30 <= nums[i] <= 30
+ 3. The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit
+    integer.
+"""
+
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        """
+        <solution type>
+
+        Complexity: <time complexity> and <space complexity>.
+        """
+        result: list[int] = []
+
+        return result

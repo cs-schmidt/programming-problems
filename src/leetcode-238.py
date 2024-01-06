37aa72583137568587cff4f1fde2fe8a827d498c
cs-schmidt <ebs.euler@gmail.com>
2024-01-06 16:45:36 -0700

    feat: complete leetcode problem 238

--- src/leetcode-238.py
+++ src/leetcode-238.py
@@ -8,13 +8,26 @@ Constraints:
     integer.
 """
 
+# NOTE: Improve by doing (1) and (2) in parallel.
+
+
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         """
-        <solution type>
+        Imperative and Iterative Solution
 
-        Complexity: <time complexity> and <space complexity>.
+        Complexity: O(n) time and O(n) auxiliary space.
         """
-        result: list[int] = []
+        # 1. Compute leftwards trailing product of 'nums'.
+        left_tp: list[int] = [1]
+        for i in range(1, len(nums)):
+            left_tp.append(left_tp[-1] * nums[i - 1])
 
-        return result
+        # 2. Compute rightwards trailing product of 'nums'.
+        right_tp: list[int] = [1]
+        for i in range(len(nums) - 1, 0, -1):
+            right_tp.insert(0, right_tp[0] * nums[i])
+
+        # 3. Merge leftwards and rightwards trailing products to produce
+        #    'result'.
+        return map(lambda l_num, r_num: l_num * r_num, left_tp, right_tp)

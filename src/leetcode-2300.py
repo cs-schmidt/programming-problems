d5142bbfdecb26783a928fca3c86cc57bf21f1b0
cs-schmidt <ebs.euler@gmail.com>
2024-01-07 14:05:22 -0700

    feat: checkpoint leetcode problem 2300

--- src/leetcode-2300.py
+++ src/leetcode-2300.py
@@ -9,6 +9,8 @@ Constraints:
  5. 1 <= success <= 10^10
 """
 
+from math import floor, ceil
+
 
 class Solution:
     def successfulPairs(
@@ -17,14 +19,47 @@ class Solution:
         """
         Imperative and Iterative Solution
 
-        Complexity: O(spells * potions) and O(spells) auxiliary space.
+        Complexity:
+         Time  - O(max(potions*log(potions),spells*log(potions))) time.
+         Space - O(spells) auxiliary space.
         """
         result: list[int] = []
-
+        potions.sort()
+        print(f'potions: {potions}\n')
         for spell in spells:
-            pairs = 0
-            for potion in potions:
-                if spell * potion >= success: pairs += 1
-            result.append(pairs)
+            successful_pairs = 0
+            min_strength = ceil(success / spell)
+            min_potion = self.least_geq(potions, min_strength)
+            if min_potion != None:
+                successful_pairs = len(potions) - min_potion
+            result.append(successful_pairs)
+            print('Iter: ')
+            print('=' * 30)
+            print(f'min_strength: {min_strength}')
+            print(f'min_potion: {min_potion or None}')
+            print(f'succesful_pairs: {successful_pairs}')
+            print('\n')
+        return result
+
+    def least_geq(self, arr: list[int], target: int) -> int:
+        """
+        Returns the index of the smallest x in 'arr', such that 'x >= target'.
+        If such a value doesn't exist, then 'None' is returned instead.
+        """
+        if not (target <= arr[len(arr) - 1]): return None
+
+        high, low = len(arr) - 1, 0
+        result = high
+        while low <= high:
+            mid = floor((high + low) / 2)
+            if arr[mid] == target:
+                result = mid
+                high = mid - 1
+            elif arr[mid] < target:
+                low = mid + 1
+            else:
+                if arr[mid] <= arr[result]:
+                    result = mid
+                high = mid - 1
 
         return result

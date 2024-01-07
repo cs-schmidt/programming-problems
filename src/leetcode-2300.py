83b7f18489346922b4844f0cdbcf8da5306df099
cs-schmidt <ebs.euler@gmail.com>
2024-01-06 23:27:33 -0700

    feat: checkpoint leetcode problem 2300

---
+++ src/leetcode-2300.py
@@ -0,0 +1,24 @@
+"""
+Problem 2300: Successful Pairs of Spells and Potions
+
+Constraints:
+ 1. n == spells.length
+ 2. m == potions.length
+ 3. 1 <= n, m <= 10^5
+ 4. 1 <= spells[i], potions[i] <= 10^5
+ 5. 1 <= success <= 10^10
+"""
+
+
+class Solution:
+    def successfulPairs(
+        self, spells: list[int], potions: list[int], success: int
+    ) -> list[int]:
+        """
+        <solution type>
+
+        Complexity: <time complexity> and <space complexity>.
+        """
+        result: list[int] = []
+
+        return result

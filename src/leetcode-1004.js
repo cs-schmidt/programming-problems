d0fcfbf6cad9d40c33e67fa6e48abc1732addc16
cs-schmidt <ebs.euler@gmail.com>
2024-01-13 11:44:53 -0700

    feat: checkpoint leetcode problem 1004

--- src/leetcode-1004.js
+++ src/leetcode-1004.js
@@ -8,9 +8,9 @@
  */
 
 /**
- * <solution type>
+ * Imperative and Iterative Solution
  *
- * Complexity: <time complexity> and <space complexity>.
+ * Complexity: O(n) time and O(1) auxiliary space.
  */
 /**
  * @param {number[]} bits
@@ -18,7 +18,21 @@
  * @return {number}
  */
 function longestOnes(bits, k) {
-  const result = 0;
+  let result = 1;
+  const chainStart = 0;
+  let chainEnd = 0;
+  let zerosInChain = bits.at(0) === 0 ? 1 : 0;
+  const bitsEnd = bits.length - 1;
+
+  // Initializes the sliding chain of ones. [O(n) time]
+  while (zerosInChain !== k && chainEnd !== bitsEnd) {
+    if (bits.at(chainEnd + 1) === 0) zerosInChain += 1;
+    chainEnd += 1;
+  }
+  while (chainEnd !== bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
+  result = Math.max(chainEnd - chainStart + 1, result);
+
+  // TODO: Slides chain through bits, checking each chain of ones we can form.
 
   return result;
 }

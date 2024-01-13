d88ad9f84e64b26701cbc1e17e90a347a7215452
cs-schmidt <ebs.euler@gmail.com>
2024-01-13 16:05:05 -0700

    feat: complete leetcode problem 1004

--- src/leetcode-1004.js
+++ src/leetcode-1004.js
@@ -22,33 +22,49 @@ function longestOnes(bits, k) {
   let chainStart = 0;
   let chainEnd = 0;
   const bitsEnd = bits.length - 1;
-
-  // TODO: Handle the k === 0 case.
-
-  // Initializes the sliding chain of ones, moving 'chainEnd' just behind the
-  // (k+1)th zero in 'bits'. [O(n) time]
-  let zerosInChain = bits.at(0) === 0 ? 1 : 0;
-  while (zerosInChain < k && chainEnd < bitsEnd) {
-    if (bits.at(chainEnd + 1) === 0) zerosInChain += 1;
-    chainEnd += 1;
-  }
-  while (chainEnd < bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
-  result = Math.max(chainEnd - chainStart + 1, result);
-
-  // Goes through all the possible chains of ones. [O(n) time]
-  while (chainEnd < bitsEnd) {
-    // Moves 'chainStart' to the start of the next longest ones sequence.
-    while (bits.at(chainStart) === 1) chainStart += 1;
-    if (bits.at(chainStart + 1) === 0) chainStart += 1;
-    else {
-      while (bits.at(chainStart) === 0) chainStart += 1;
-    }
-    // Moves 'chainEnd' to the end of the next longest ones sequence.
-    chainEnd += 1;
-    while (chainEnd < bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
-    // Update the result.
+  if (k === 0) {
+    // Initializes the sliding chain of ones, moving 'chainStart' to the start
+    // and 'chainEnd' to the end of the next longest sequence of ones in 'bits'.
+    // [O(n) time]
+    while (chainStart < bitsEnd && bits.at(chainStart) !== 1) chainStart += 1;
+    chainEnd = chainStart;
+    while (chainEnd < bitsEnd && bits.at(chainEnd + 1) !== 0) chainEnd += 1;
+    if (chainStart === chainEnd && bits.at(chainEnd) === 0) return result;
     result = Math.max(chainEnd - chainStart + 1, result);
+    // Goes through all the possible chains of ones. [O(n) time]
+    while (chainEnd < bitsEnd) {
+      while (chainEnd < bitsEnd && bits.at(chainEnd + 1) === 0) chainEnd += 1;
+      if (chainEnd < bitsEnd) {
+        chainEnd += 1;
+        chainStart = chainEnd;
+        while (chainEnd < bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
+        result = Math.max(chainEnd - chainStart + 1, result);
+      } else return result;
+    }
+  } else {
+    // Initializes the sliding chain of ones, moving 'chainEnd' just behind the
+    // (k+1)th zero in 'bits'. [O(n) time]
+    let zerosInChain = bits.at(0) === 0 ? 1 : 0;
+    while (chainEnd < bitsEnd && zerosInChain < k) {
+      if (bits.at(chainEnd + 1) === 0) zerosInChain += 1;
+      chainEnd += 1;
+    }
+    while (chainEnd < bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
+    result = Math.max(chainEnd - chainStart + 1, result);
+    // Goes through all the possible chains of ones. [O(n) time]
+    while (chainEnd < bitsEnd) {
+      // Moves 'chainStart' to the start of the next longest ones sequence.
+      while (bits.at(chainStart) === 1) chainStart += 1;
+      if (bits.at(chainStart + 1) === 0) chainStart += 1;
+      else {
+        while (bits.at(chainStart) === 0) chainStart += 1;
+      }
+      // Moves 'chainEnd' to the end of the next longest ones sequence.
+      chainEnd += 1;
+      while (chainEnd < bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
+      // Update the result.
+      result = Math.max(chainEnd - chainStart + 1, result);
+    }
   }
-
   return result;
 }

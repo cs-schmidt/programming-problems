b354af80e2d3be2d87d07c10e185c35b7e925887
cs-schmidt <ebs.euler@gmail.com>
2024-01-13 16:17:52 -0700

    feat: checkpoint leetcode problem 2130

---
+++ src/leetcode-2130.ts
@@ -0,0 +1,28 @@
+/**
+ * Problem 2130: Maximum Twin Sum of a Linked List
+ *
+ * Constraints:
+ *  1. The number of nodes in the list is an even integer in the range [2, 10^5].
+ *  2. 1 <= Node.val <= 105
+ */
+
+/** Represents a node in a singly-linked list. */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number = 0, next: ListNode | null = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+/**
+ * <solution type>
+ *
+ * Complexity: <time complexity> and <space complexity>.
+ */
+function pairSum(head: ListNode): number {
+  const result = 0;
+
+  return result;
+}

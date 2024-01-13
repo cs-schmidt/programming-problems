83c23d67fc812a69cf451434c5552b00e191aeec
cs-schmidt <ebs.euler@gmail.com>
2024-01-12 18:14:02 -0700

    feat: checkpoint leetcode problem 1679

---
+++ src/leetcode-1679.py
@@ -0,0 +1,19 @@
+"""
+Problem 1679: Max Number of K-Sum Pairs
+
+Constraints:
+ 1. 1 <= nums.length <= 10^5
+ 2. 1 <= nums[i] <= 10^9
+ 3. 1 <= k <= 10
+"""
+
+class Solution:
+    def maxOperations(self, nums: list[int], k: int) -> int:
+        """
+        <solution type>
+
+        Complexity: <time complexity> and <space complexity>.
+        """
+        result = 0
+
+        return result

4baa1935a551607d3fa31253b8066f0c77650630
cs-schmidt <ebs.euler@gmail.com>
2024-01-13 15:32:40 -0700

    feat: checkpoint leetcode problem 1004

--- src/leetcode-1004.js
+++ src/leetcode-1004.js
@@ -18,21 +18,37 @@
  * @return {number}
  */
 function longestOnes(bits, k) {
-  let result = 1;
-  const chainStart = 0;
+  let result = 0;
+  let chainStart = 0;
   let chainEnd = 0;
-  let zerosInChain = bits.at(0) === 0 ? 1 : 0;
   const bitsEnd = bits.length - 1;
 
-  // Initializes the sliding chain of ones. [O(n) time]
-  while (zerosInChain !== k && chainEnd !== bitsEnd) {
+  // TODO: Handle the k === 0 case.
+
+  // Initializes the sliding chain of ones, moving 'chainEnd' just behind the
+  // (k+1)th zero in 'bits'. [O(n) time]
+  let zerosInChain = bits.at(0) === 0 ? 1 : 0;
+  while (zerosInChain < k && chainEnd < bitsEnd) {
     if (bits.at(chainEnd + 1) === 0) zerosInChain += 1;
     chainEnd += 1;
   }
-  while (chainEnd !== bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
+  while (chainEnd < bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
   result = Math.max(chainEnd - chainStart + 1, result);
 
-  // TODO: Slides chain through bits, checking each chain of ones we can form.
+  // Goes through all the possible chains of ones. [O(n) time]
+  while (chainEnd < bitsEnd) {
+    // Moves 'chainStart' to the start of the next longest ones sequence.
+    while (bits.at(chainStart) === 1) chainStart += 1;
+    if (bits.at(chainStart + 1) === 0) chainStart += 1;
+    else {
+      while (bits.at(chainStart) === 0) chainStart += 1;
+    }
+    // Moves 'chainEnd' to the end of the next longest ones sequence.
+    chainEnd += 1;
+    while (chainEnd < bitsEnd && bits.at(chainEnd + 1) === 1) chainEnd += 1;
+    // Update the result.
+    result = Math.max(chainEnd - chainStart + 1, result);
+  }
 
   return result;
 }

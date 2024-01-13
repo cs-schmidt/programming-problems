279ab4745f89916fb1b813f5f4f7d949601e5d7f
cs-schmidt <ebs.euler@gmail.com>
2024-01-12 21:50:39 -0700

    feat: checkpoint leetcode problem 1004

---
+++ src/leetcode-1004.js
@@ -0,0 +1,24 @@
+/**
+ * Problem 1004: Max Consecutive Ones III
+ *
+ * Constraints:
+ *  1. 1 <= nums.length <= 10^5
+ *  2. nums[i] is either 0 or 1.
+ *  3. 0 <= k <= nums.length
+ */
+
+/**
+ * <solution type>
+ *
+ * Complexity: <time complexity> and <space complexity>.
+ */
+/**
+ * @param {number[]} bits
+ * @param {number} k
+ * @return {number}
+ */
+function longestOnes(bits, k) {
+  const result = 0;
+
+  return result;
+}

35146b8a2804d04a249d50ce75767be7a85932f1
cs-schmidt <ebs.euler@gmail.com>
2024-01-13 19:25:51 -0700

    feat: checkpoint leetcode problem 334

---
+++ src/leetcode-334.js
@@ -0,0 +1,22 @@
+/**
+ * Problem 334: Increasing Triplet Subsequence
+ *
+ * Constraints:
+ *  1. 1 <= nums.length <= 5 * 10^5
+ *  2. -2^31 <= nums[i] <= 2^31 - 1
+ */
+
+/**
+ * <solution type>
+ *
+ * Complexity: <time complexity> and <space complexity>.
+ */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function increasingTriplet(nums) {
+  var result = false;
+
+  return result;
+}

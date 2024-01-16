ece8de1aa6a5630c61dd489f58b093d3ec49934e
cs-schmidt <ebs.euler@gmail.com>
2024-01-16 13:01:24 -0700

    feat: complete leetcode problem 2631

---
+++ src/leetcode-2631.js
@@ -0,0 +1,26 @@
+/**
+ * Problem 2631: Group By
+ *
+ * Constraints:
+ *  1. 0 <= array.length <= 10^5
+ *  2. fn returns a string
+ */
+
+/**
+ * Declarative and Iterative Solution
+ *
+ * Complexity: O(n) time and O(n) auxiliary space.
+ */
+/**
+ * @param {Function}
+ * @return {Object}
+ */
+Array.prototype.groupBy = function (fn) {
+  const result = {};
+  this.forEach((val) => {
+    const key = fn(val);
+    if (Object.hasOwn(result, key)) result[key].push(val);
+    else result[key] = [val];
+  });
+  return result;
+};

4a3e208c6ba58f4843f09af51f2258e08b9c0341
cs-schmidt <ebs.euler@gmail.com>
2024-01-15 20:06:19 -0700

    feat: complete leetcode problem 2667

---
+++ src/leetcode-2667.js
@@ -0,0 +1,18 @@
+/**
+ * Problem 2667: Create Hello World Function
+ *
+ * Constraints:
+ *  1. 0 <= args.length <= 10
+ */
+
+/**
+ * Declarative and Iterative Solution
+ *
+ * Complexity: O(1) time and O(1) auxiliary space.
+ */
+/**
+ * @return {Function}
+ */
+function createHelloWorld() {
+  return () => 'Hello World';
+}

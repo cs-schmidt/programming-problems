5ddb78eadf9d7c12fc51814ca373645d0003077a
cs-schmidt <ebs.euler@gmail.com>
2024-01-15 21:46:57 -0700

    feat: complete leetcode problem 2626

---
+++ src/leetcode-2626.js
@@ -0,0 +1,24 @@
+/**
+ * Problem 2626: Array Reduce Transformation
+ *
+ * Constraints:
+ *  1. 0 <= nums.length <= 1000
+ *  2. 0 <= nums[i] <= 1000
+ *  3. 0 <= init <= 1000
+ */
+
+/**
+ * Declarative and Iterative Solution
+ *
+ * Complexity: O(n) time and O(1) auxiliary space.
+ */
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number[]}
+ */
+function reduce(arr, fn, init) {
+  arr.forEach((val) => (init = fn(init, val)));
+  return init;
+}

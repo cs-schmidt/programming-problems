8a86b765a7a861d248407a2b350d717c98fa7cc9
cs-schmidt <ebs.euler@gmail.com>
2024-01-17 14:33:47 -0700

    feat: checkpoint leetcode problem 459

---
+++ src/leetcode-459.py
@@ -0,0 +1,16 @@
+"""
+Problem 459: Repeated Substring Pattern
+
+Constraints:
+ 1. 1 <= s.length <= 10^4
+ 2. s consists of lowercase English letters.
+"""
+
+class Solution:
+    def repeatedSubstringPattern(self, str: str) -> bool:
+        """
+        <solution type>
+
+        Complexity: <time complexity> and <space complexity>.
+        """
+        return True

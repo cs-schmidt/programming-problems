9b412f752a0347fee9291fbc27a81a43e96c4e83
cs-schmidt <ebs.euler@gmail.com>
2024-01-18 15:54:40 -0700

    feat: complete leetcode problem 334

--- src/leetcode-334.js
+++ src/leetcode-334.js
@@ -6,13 +6,10 @@
  *  2. -2^31 <= nums[i] <= 2^31 - 1
  */
 
-// TODO: Complete this problem.
+// TODO: Improve performance and readability.
 
 /**
- * Imperative and Iterative Solution
- *
- * Applies a "limited dynamic programming" approach in a manner similar to the
- * Longest Increasing Subsequence Problem, going bottom-up.
+ * Declarative and Iterative Solution
  *
  * Complexity: O(n) time and O(1) auxiliary space.
  */
@@ -21,30 +18,41 @@
  * @return {boolean}
  */
 function increasingTriplet(nums) {
-  if (nums.length < 3) return false;
-  const sequence = [nums.at(0)];
-  let index = 0;
-  while (index < nums.length && nums.at(index) <= sequence.at(-1)) {
-    sequence[sequence.length - 1] = nums.at(index);
-    index += 1;
-  }
-  if (index < nums.length) {
-    sequence[sequence.length - 1] = nums.at(index);
-    index += 1;
-  }
-  while (index < nums.length && nums.length < 3) {
-    if (nums.at(index) < nums.at(0)) {
-      //
-    } else if (
-      sequence.at(0) < nums.at(index) &&
-      nums.at(index) < sequence.at(-1)
-    ) {
-      sequence.length -= 1;
-      sequence.push(nums.at(index));
-    } else if (nums.at(index) > sequence.at(-1)) {
-      sequence.push(nums.at(index));
+  let low = findFirstIncrease(0);
+  if (Number.isNaN(low)) return false;
+  let high = low + 1;
+  while (true) {
+    const nextLow = findFirstIncrease(high);
+    if (Number.isNaN(nextLow)) return false;
+    const nextHigh = nextLow + 1;
+    if (nums[nextHigh] > nums[high] || nums[nextLow] > nums[low]) return true;
+    if (nums[nextHigh] > nums[low]) {
+      if (
+        nums
+          .slice(high + 1, nextLow)
+          .find((n) => n > nums[low] && n < nums[high])
+      )
+        return true;
     }
-    index += 1;
+    low = nums[low] <= nums[nextLow] ? low : nextLow;
+    high = nextHigh;
   }
-  return sequence.length >= 3;
+
+  /**
+   * From 'start' find the first index on `nums` where the element at that index
+   * is less than the element after it. Otherwise, we return -1 when there is no
+   * such case.
+   * @param {number|undefined} start - The place where the search begins.
+   * @return {number}
+   */
+  function findFirstIncrease(start) {
+    if (start === undefined) start = 0;
+    else if (typeof start !== 'number') throw TypeError();
+    if (start < 0 || start > nums.length - 2) return NaN;
+    while (start <= nums.length - 2) {
+      if (nums[start] < nums[start + 1]) return start;
+      start += 1;
+    }
+    return NaN;
+  }
 }

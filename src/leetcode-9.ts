bc2b72957a2af50d388e3890180d77c9fe2b473e
cs-schmidt <ebs.euler@gmail.com>
2024-01-18 16:39:46 -0700

    feat: complete leetcode problem 9

---
+++ src/leetcode-9.ts
@@ -0,0 +1,21 @@
+/**
+ * Problem 9: Palindrome Number
+ *
+ * Constraints:
+ *  1. -2^31 <= x <= 2^31 - 1
+ */
+
+// TODO: Complete the problem's follow up portion.
+
+/**
+ * Imperative and Iterative Solution
+ *
+ * Complexity: O(log_10(x)) time and O(log_10(x)) auxiliary space.
+ */
+function isPalindrome(x: number): boolean {
+  const stringNum = x.toString();
+  for (let i = 0; i <= Math.floor((stringNum.length - 1) / 2); i++) {
+    if (stringNum.at(i) !== stringNum.at(-i - 1)) return false;
+  }
+  return true;
+}

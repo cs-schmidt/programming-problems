76a1fde433d2d6f973b2f6061e67ec66ed401c6d
cs-schmidt <ebs.euler@gmail.com>
2024-01-19 11:08:37 -0700

    feat: update leetcode problem 215

--- src/leetcode-215.py
+++ src/leetcode-215.py
@@ -6,21 +6,16 @@ Constraints:
  2. -10^4 <= nums[i] <= 10^4
 """
 
-# NOTE: There may be a method with a modified version of the randomized-select
-#       algorithm.
-
 import heapq
+
 
 class Solution:
     def findKthLargest(self, nums: list[int], k: int) -> int:
         """
         Iterative and Imperative Solution
 
-        Complexity: <time complexity> and O(n) auxiliary space.
+        Complexity: O(n) time and O(1) auxiliary space.
         """
-        heap = nums.copy()
-        heapq.heapify(heap)
-
-        for _ in range(len(heap) - k): heapq.heappop(heap) 
-
-        return heapq.heappop(heap)
+        heapq.heapify(nums)
+        for _ in range(len(nums) - k):  heapq.heappop(nums)
+        return nums[0]

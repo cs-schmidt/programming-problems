a8d808838a2559f68d1591c20c14e44047fde685
cs-schmidt <ebs.euler@gmail.com>
2024-07-06 03:10:18 -0600

    feat: improve leetcode problem 2462 solution

--- src/leetcode-2462.ts
+++ src/leetcode-2462.ts
@@ -6,32 +6,39 @@
  *  2. 1 <= k, candidates <= costs.length
  */
 
-// TODO: Improve time complexity.
-
 /**
  * Imperative, Iterative, and Pure Solution.
  *
- * Time complexity: (?).
+ * Time complexity: O(k * log(candidates)).
  * Space complexity: O(candidates) auxiliary space.
  */
 function totalCost(costs: number[], k: number, candidates: number): number {
   let result: number = 0;
-  let middleLow: number = candidates;
-  let middleHigh: number = getMiddleHigh();
-  const lowerCosts: number[] = costs.slice(0, middleLow);
-  const upperCosts: number[] = costs.slice(middleHigh + 1);
+  let middleHead: number = candidates;
+  let middleTail: number =
+    costs.length - Math.min(candidates, costs.length - candidates) - 1;
+  const lowerCosts: number[] = costs.slice(0, middleHead);
+  const upperCosts: number[] = costs.slice(middleTail + 1);
   heapify(lowerCosts);
   heapify(upperCosts);
   while (k--) {
     if (lowerCosts[0] <= (upperCosts[0] ?? Infinity)) {
-      result += lowerCosts.shift();
-      if (middleLow <= middleHigh) lowerCosts.unshift(costs[middleLow++]);
-      else if (lowerCosts.length) lowerCosts.unshift(lowerCosts.pop());
+      if (middleHead <= middleTail) {
+        result += lowerCosts[0];
+        lowerCosts[0] = costs[middleHead++];
+      } else {
+        swapEnds(lowerCosts);
+        result += lowerCosts.pop();
+      }
       siftDown(lowerCosts);
     } else {
-      result += upperCosts.shift();
-      if (middleLow <= middleHigh) upperCosts.unshift(costs[middleHigh--]);
-      else if (upperCosts.length) upperCosts.unshift(upperCosts.pop());
+      if (middleHead <= middleTail) {
+        result += upperCosts[0];
+        upperCosts[0] = costs[middleTail--];
+      } else {
+        swapEnds(upperCosts);
+        result += upperCosts.pop();
+      }
       siftDown(upperCosts);
     }
   }
@@ -39,17 +46,19 @@ function totalCost(costs: number[], k: number, candidates: number): number {
 
   // Internal Helper Functions
   // **************************************************
-  function getMiddleHigh() {
-    return costs.length - Math.min(candidates, costs.length - candidates) - 1;
+  /** Swaps the first and last elements in an array. */
+  function swapEnds(nums: number[]): void {
+    if (nums.length)
+      [nums[0], nums[nums.length - 1]] = [nums[nums.length - 1], nums[0]];
   }
 
-  /** Transforms the array of numbers into a min-heap. */
+  /** Transforms a number array into a min-heap. */
   function heapify(nums: number[]): void {
     let index: number = Math.floor((nums.length - 1) / 2);
     while (index >= 0) siftDown(nums, index--);
   }
 
-  /** Does a sift down operation on an array of numbers starting from `i`. */
+  /** Does a Sifts down on a number array starting at `i`. */
   function siftDown(nums: number[], i: number = 0): void {
     let min: number = i;
     let left: number = 2 * i + 1;

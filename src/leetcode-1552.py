ed4059fd7d214b4377e9a95682b05045c1cb43f1
cs-schmidt <ebs.euler@gmail.com>
2024-07-06 14:23:50 -0600

    feat: checkpoint leetcode problem 1552

--- src/leetcode-1552.py
+++ src/leetcode-1552.py
@@ -9,21 +9,56 @@ Constraints:
 
 # TODO: Complete solution.
 
-from math import ceil
-
 
 class Solution:
     def maxDistance(self, position: list[int], m: int) -> int:
         """
-        Imperative, Iterative, and Impure Solution
+        Imperative, Iterative, and Pure Solution
 
-        Time complexity: len(position)*log(len(position)).
+        Time complexity: O(|position|*log(|position|).
         Space complexity: O(1) auxiliary space.
         """
-        position.sort()
-        # I see the following strategies (which may be used in tandem) for this
-        # problem:
-        # 
+        # I see these as the strategies for this problem:
         #  - Binary Search
         #  - Greedy Search
-        #  - Sliding Window 
+        #  - Sliding Window
+
+        # Internal Helper Functions
+        # ************************************************************
+        def findContiguousMax(nums: list[int], size: int):
+            """
+            Finds the maximum sum amongst all subarrays of the given `size` in
+            `nums`.
+            """
+            if size < 0 or size > len(nums):
+                raise ValueError(f"{size} is outside of the allowable range.")
+            result = 0
+            for i in range(size):
+                result += nums[i]
+            runningSum = result
+            for i in range(size, len(nums)):
+                runningSum += nums[i]
+                runningSum -= nums[i - size]
+                if runningSum > result:
+                    result = runningSum
+            return result
+
+        # Main Logic
+        # ************************************************************
+        result = 0
+        position.sort()
+        gaps = [position[i + 1] - position[i] for i in range(len(position) - 1)]
+        min_gap_size = min(gaps)
+        max_gap_size = findContiguousMax(gaps, len(gaps) - m + 1)
+        while min_gap_size < max_gap_size:
+            gap_size = (max_gap_size + min_gap_size) // 2
+            # Check if a distribution for `gap_size` can be found.
+            distribution_found = False
+            if distribution_found:
+                result = gap_size
+                min_gap_size = gap_size + 1
+                continue
+            else:
+                max_gap_size = gap_size - 1
+                continue
+        return result

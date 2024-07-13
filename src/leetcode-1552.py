e438aa2307028ede45eedb412579142093a410b3
cs-schmidt <ebs.euler@gmail.com>
2024-07-12 19:05:08 -0600

    feat: checkpoint leetcode problem 1552

--- src/leetcode-1552.py
+++ src/leetcode-1552.py
@@ -4,44 +4,36 @@ Problem 1552: Magnetic Force Between Two Balls
 Constraints:
  1. 2 <= m <= |position| <= 10^5
  2. 1 <= position[i] <= 10^9
- 3. position[i] != position[j] for any i,j.
+ 3. position[i] != position[j] for all i,j where i != j.
 """
 
 # TODO: Complete solution.
+
+from itertools import islice
 
 
 class Solution:
     def maxDistance(self, position: list[int], m: int) -> int:
         """
-        Imperative, Iterative, and Pure Solution
+        Imperative, Iterative, and Impure Solution
 
         Time complexity: O(|position|*log(|position|).
-        Space complexity: O(1) auxiliary space.
+        Space complexity: O(|position|) auxiliary space.
         """
-        # I see these as the strategies for this problem:
-        #  - Binary Search
-        #  - Greedy Search
-        #  - Sliding Window
 
-        # Internal Helper Functions
+        # Internal Helpers
         # ************************************************************
-        def findContiguousMax(nums: list[int], size: int):
-            """
-            Finds the maximum sum amongst all subarrays of the given `size` in
-            `nums`.
-            """
-            if size < 0 or size > len(nums):
-                raise ValueError(f"{size} is outside of the allowable range.")
-            result = 0
-            for i in range(size):
-                result += nums[i]
-            runningSum = result
-            for i in range(size, len(nums)):
-                runningSum += nums[i]
-                runningSum -= nums[i - size]
-                if runningSum > result:
-                    result = runningSum
+        def max_neighbor_gap(gaps: list[int], neighbor_spaces: int):
+            slice_size: int = len(gaps) - (neighbor_spaces - 1)
+            slice_sum: int = sum(islice(gaps, slice_size))
+            result: int = slice_sum
+            for i in range(len(gaps) - slice_size):
+                slice_sum += gaps[slice_size + i] - gaps[i]
+                result = max(result, slice_sum)
             return result
+
+        def last_index(arr: list) -> int:
+            return len(arr) - 1 if arr else 0
 
         # Main Logic
         # ************************************************************
@@ -49,16 +41,30 @@ class Solution:
         position.sort()
         gaps = [position[i + 1] - position[i] for i in range(len(position) - 1)]
         min_gap_size = min(gaps)
-        max_gap_size = findContiguousMax(gaps, len(gaps) - m + 1)
+        max_gap_size = max_neighbor_gap(gaps, m - 1)
         while min_gap_size < max_gap_size:
-            gap_size = (max_gap_size + min_gap_size) // 2
-            # Check if a distribution for `gap_size` can be found.
-            distribution_found = False
-            if distribution_found:
-                result = gap_size
-                min_gap_size = gap_size + 1
-                continue
-            else:
-                max_gap_size = gap_size - 1
-                continue
+            # Find the firsts slice on `gaps` whose sum is closest to the
+            # midpoint between `min_gap_size` and `max_gap_size`.
+            mid_gap_size = (min_gap_size + max_gap_size) // 2
+            slice_sum = gaps[0]
+            slice_head = 0
+            slice_tail = 0
+            nearest_slice_sum = 0
+            nearest_slice_head = 0
+            nearest_slice_tail = 0
+            while slice_sum != mid_gap_size and slice_head < last_index(gaps):
+                # TODO: Implement this part.
+                a
+            # Check if `nearest_slice_sum` is a possible solution.
+            rear_gaps = 0  # TODO: Initalize this.
+            front_gaps = 0  # List of <gap_tail, gap_size> pairs.
+            while (
+                rear_gaps + front_gaps + 1 < m - 1
+                and nearest_slice_head < last_index(gaps)
+            ):
+                # TODO: Implement this part.
+                a
+            if rear_gaps + front_gaps + 1 == m - 1:
+                min_gap_size = nearest_slice_sum + 1
+                result = nearest_slice_sum
         return result

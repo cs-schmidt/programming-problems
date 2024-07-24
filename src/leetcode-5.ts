4e20cbe8d671ac7464a559d17d22f5a6ca5b1e70
cs-schmidt <ebs.euler@gmail.com>
2024-07-24 02:07:50 -0600

    feat: checkpoint leetcode problem 5

---
+++ src/leetcode-5.ts
@@ -0,0 +1,19 @@
+/**
+ * Problem 5: Longest Palindromic Substring
+ * Constraints:
+ *  1. 1 <= s.length <= 1000
+ *  2. s consist of only digits and English letters.
+ */
+
+// TODO: Complete solution.
+
+/**
+ * Imperative, Iterative, and Pure Solution
+ *
+ * Time Complexity: O(?).
+ * Space Complexity: O(?) auxiliary space.
+ */
+function longestPalindrome(s: string): string {
+  const result = '';
+  return result;
+}
